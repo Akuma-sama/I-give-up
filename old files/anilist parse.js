7c5c6950d5b1db85fa164b51d8717017781865ca
Akuma <87880680+Akuma-sama@users.noreply.github.com>
2021-11-08 10:07:44 -0500

    Update anilist parse.js

--- old files/anilist parse.js
+++ old files/anilist parse.js
@@ -2,44 +2,81 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 links = [
-    'https://anilist.co/anime/133965/Komisan-wa-Komyushou-desu/',
-    'https://anilist.co/anime/131573/Jujutsu-Kaisen-0/',
-    'https://anilist.co/anime/9253/SteinsGate/'
+    'https://anilist.co/anime/133965/',
+    'https://anilist.co/anime/131573/',
+    'https://anilist.co/anime/9253/'
 
 ];
-
-for(let i=0; i<links.length; i++) {
-    axios.get(links[i])
+//#region 
+// for(let i=0; i<links.length; i++) {
+    axios.get(links[0])
         .then( response => {
             const html = response.data;
             const $ = cheerio.load(html);
-            const linkData = [{data: links[i]}];
+            let linkData = [{data: links[0]}];
 
             $('.data-set', html).each(function () {
                 const type = $(this).children('.type').text();
                 const value = $(this).children('.value').text();
-
+                let i=0
+                i++
                 linkData.push({
-                    type: type,
-                    value: value
+                    i: {
+                        type: type,
+                        value: value
+                    }
                 });
             });
 
-            $('.tags', html).each(function () {
-                const tagNames = $(this).children('.tag').children('.name').text();
-                linkData.push({
-                    tags: tagNames,
-                });
-            });
+//             $('.tags', html).each(function () {
+//                 const tagNames = $(this).children('.tag').children('.name').text();
+//                 linkData.push({
+//                     tags: tagNames,
+//                 });
+//             });
 
-            const coverURL = $('.cover').attr('src');
-            const description = $('.description').text();
-            linkData.push({
-                coverURL,
-                description
-            });
+//             const coverURL = $('.cover').attr('src');
+//             const description = $('.description').text();
+//             linkData.push({
+//                 coverURL,
+//                 description
+//             });
 
             console.log(linkData);
 
         }).catch(error => console.log(error));
-};
+// };
+//#endregion
+
+
+
+
+
+
+
+
+// Didnt work and made no sense
+// axios.get(links[0])
+//     .then( response => {
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+//         // All the data variables so they are local to this function
+//         let link;
+//         let title;
+//         let nativeTitle;
+//         let description;
+//         let status;
+//         let episodeDuration;
+//         let season;
+//         let averageScore;
+//         let studio;
+//         let source;
+//         let genre;
+//         let tags;
+
+//         // title
+//         title = $('.content').text();
+
+//         console.log(title);
+
+//     }).catch(error => console.log(error));
